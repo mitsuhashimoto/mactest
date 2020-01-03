a75bf9dd85b420f51665c028f6eb7ecbaf6de2b1
橋本 光弘 <mitsu_hashi@sage.ocn.ne.jp>
2020-01-03 12:13:53 +0900

    f

---
+++ tes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
